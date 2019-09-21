b3f77bbfa93625bdead49bdc9d49643db2d99edf
usb12345 <54896617+usb12345@users.noreply.github.com>
2019-09-21 10:33:52 +0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	     Employee empl = new Employee(10000);
+        System.out.println("Enter a salary"     +10000);
+        System.out.println("Enter commission"     +10000*0.1);
+        System.out.println("Enter bonus"     +5000/4);
+
     }
 }
